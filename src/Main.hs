adf10c6d315a8af083c21d786045e3c9add94e32
Scott Fleischman <scott-fleischman@users.noreply.github.com>
2017-05-29 15:01:01 -0700

    Add example printing of shas for each branch.

--- src/Main.hs
+++ src/Main.hs
@@ -3,15 +3,18 @@
 module Main where
 
 import qualified Data.String as String
+import qualified Data.Text as Text
 import qualified GitHub
-import qualified GitHub.Endpoints.Users as GitHub
+import qualified GitHub.Endpoints.Repos as Github
 import qualified System.Environment as Env
 
 main :: IO ()
 main = do
   auth <- getAuth
-  user <- GitHub.userInfoFor' auth "scott-fleischman"
-  print user
+  possibleBranches <- Github.branchesFor' auth "scott-fleischman" "greek-grammar"
+  case possibleBranches of
+    Left error -> putStrLn $ "Error: " ++ show error
+    Right branches -> mapM_ (print . GitHub.branchCommitSha . GitHub.branchCommit) branches
 
 getAuth :: IO (Maybe (GitHub.Auth))
 getAuth = do
